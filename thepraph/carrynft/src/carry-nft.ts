95a96bd6adbe4846fbc6e0d40620df3169cbd18a
Carry Wang <design.carrywang@gmail.com>
2024-05-04 23:37:51 +0800

    Add TokenInfo entity

--- thepraph/carrynft/src/carry-nft.ts
+++ thepraph/carrynft/src/carry-nft.ts
@@ -1,38 +1,43 @@
 import {
   NFTCreated as NFTCreatedEvent,
   NFTRegesitered as NFTRegesiteredEvent,
-  OwnershipTransferred as OwnershipTransferredEvent
-} from "../generated/CarryNFT/CarryNFT"
+  OwnershipTransferred as OwnershipTransferredEvent,
+} from "../generated/CarryNFT/CarryNFT";
 import {
   NFTCreated,
   NFTRegesitered,
-  OwnershipTransferred
-} from "../generated/schema"
+  OwnershipTransferred,
+} from "../generated/schema";
+import { S2NFT } from "../generated/templates";
 
 export function handleNFTCreated(event: NFTCreatedEvent): void {
   let entity = new NFTCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nftCA = event.params.nftCA
+  );
+  entity.nftCA = event.params.nftCA;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  S2NFT.create(event.params.nftCA);
+
+  entity.save();
 }
 
 export function handleNFTRegesitered(event: NFTRegesiteredEvent): void {
   let entity = new NFTRegesitered(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nftCA = event.params.nftCA
+  );
+  entity.nftCA = event.params.nftCA;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  S2NFT.create(event.params.nftCA);
+
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -40,13 +45,13 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
---
+++ thepraph/carrynft/src/s-2-nft.ts
@@ -0,0 +1,79 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  Transfer as TransferEvent,
+  S2NFT,
+} from "../generated/S2NFT/S2NFT";
+import {
+  Approval,
+  ApprovalForAll,
+  Transfer,
+  TokenInfo,
+} from "../generated/schema";
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.owner = event.params.owner;
+  entity.approved = event.params.approved;
+  entity.tokenId = event.params.tokenId;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.owner = event.params.owner;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function updateTokenInfo(event: TransferEvent): void {
+  const id = `${event.address.toHexString()}#${event.params.tokenId.toHexString()}`;
+  let tokenInfo = TokenInfo.load(id);
+
+  if (!tokenInfo) {
+    tokenInfo = new TokenInfo(id);
+
+    const contract = S2NFT.bind(event.address);
+
+    tokenInfo.name = contract.name();
+    tokenInfo.tokenURL = contract.tokenURI(event.params.tokenId);
+    tokenInfo.tokenId = event.params.tokenId;
+    tokenInfo.ca = event.address;
+  }
+
+  tokenInfo.owner = event.params.to;
+  tokenInfo.blockNumber = event.block.number;
+  tokenInfo.blockTimestamp = event.block.timestamp;
+  tokenInfo.transactionHash = event.transaction.hash;
+  tokenInfo.save();
+}
